fb6833936e7b70e3cb2e52ca33d23496310fb304
Kalpit <kalpit677@gmail.com>
2024-04-23 17:26:33 +0530

    back commit

--- index.js
+++ index.js
@@ -1,116 +1,122 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+import express from 'express' ;
+import path from 'path';
+import mongoose  from 'mongoose';
+import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken'
 
-const uri = "mongodb+srv://kalpit677:puranpur@jrp.mpoq8pc.mongodb.net/?retryWrites=true&w=majority&appName=JRP";
-const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+//convert password into hashformate
+import bcrypt, { hash } from 'bcrypt'
 
-mongoose.connect(uri, clientOptions)
-    .then(() => console.log("Database Connected"))
-    .catch((err) => {
-        console.error("Error connecting to database:", err.message);
-        process.exit(1); // Exit the process if unable to connect
-    });
+
+
+mongoose.connect("mongodb://localhost:27017/",
+{
+dbName:"backend" ,
+}).then (()=>console.log("Database Connected"))
+.catch((e)=>console.log(e));
 
 const userSchema = new mongoose.Schema({
     name: String,
-    email: String,
-    password: String,
-});
+    email:String,
+    password:String,
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema)
 
 const app = express();
 
-app.use(express.static(path.join(path.resolve(), "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(path.resolve(),"public")))
+
+//Using middlewares
+app.use(express.urlencoded({extended :true}))
 app.use(cookieParser());
+
+//setting up view engine
 app.set("view engine", "ejs");
 
+//middleware-handler
 const isAuthenticated = async (req, res, next) => {
     const { token } = req.cookies;
 
-    if (token) {
-        try {
-            const decoded = jwt.verify(token, "santiobskabkjdn");
-            req.user = await User.findById(decoded._id);
-            next();
-        } catch (err) {
-            res.render("login");
-        }
+    if(token) {
+        const decoded = jwt.verify(token, "santiobskabkjdn");
+        req.user = await User.findById(decoded._id);
+        next();
     } else {
         res.render("login");
     }
-};
+}
 
-app.get("/", isAuthenticated, (req, res) => {
+app.get("/", isAuthenticated,(req,res)=>{
     console.log(req.user);
-    res.render("logout", { name: req.user.name });
+    res.render("logout",{name:req.user.name});
 });
 
-app.get("/login", (req, res) => {
+app.get("/login",(req,res)=>{
     res.render("login");
 });
 
-app.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    let user = await User.findOne({ email });
+app.post("/login",async(req,res)=>{
+    const {email,password} = req.body;
+    let user = await User.findOne({email});
 
-    if (!user) return res.redirect("/register");
+    if(!user) return res.redirect("/register");
+ //check password is correct or not
+    const isMatch = await bcrypt.compare(password,user.password);
+    if(!isMatch) return res.render("login",{message:"Incorrect Password"});
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.render("login", { message: "Incorrect Password" });
-
-    const token = jwt.sign({ _id: user._id }, "santiobskabkjdn");
-    res.cookie("token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 60 * 1000)
+    const token = jwt.sign({_id:user._id},"santiobskabkjdn");
+    res.cookie("token",token,{
+        httpOnly:true,
+        expires:new Date(Date.now()+60*1000)
     });
 
-    res.redirect("/");
-});
+    res.redirect("/")
+})
 
-app.get("/register", async (req, res, next) => {
+app.get("/register", async(req,res,next)=>{
+
     res.render("register");
 });
 
-app.post("/register", async (req, res) => {
-    const { name, email, password } = req.body;
+app.post("/register",async(req,res) =>{
 
-    let user = await User.findOne({ email });
-    if (user) {
-        return res.redirect("/login");
+    const {name,email,password} = req.body;
+
+    let user = await User.findOne({email});
+    if(user){
+      return res.redirect("/login");
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password,10);
+
 
     user = await User.create({
         name,
         email,
-        password: hashedPassword,
+        password:hashedPassword,
+    })
+
+    const token = jwt.sign({_id:user._id},"santiobskabkjdn");
+    res.cookie("token",token,{
+        httpOnly:true,
+        expires:new Date(Date.now()+60*1000)
     });
 
-    const token = jwt.sign({ _id: user._id }, "santiobskabkjdn");
-    res.cookie("token", token, {
-        httpOnly: true,
-        expires: new Date(Date.now() + 60 * 1000)
+    res.redirect("/")
+})
+
+app.get("/logout",(req,res) =>{
+    res.cookie("token",null,{
+        httpOnly:true,
+        expires:new Date(Date.now())
     });
 
-    res.redirect("/");
-});
+    res.redirect("/")
+})
 
-app.get("/logout", (req, res) => {
-    res.cookie("token", null, {
-        httpOnly: true,
-        expires: new Date(Date.now())
-    });
 
-    res.redirect("/");
-});
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
+app.listen(5000,()=>{
+    console.log("server is working");
+})
